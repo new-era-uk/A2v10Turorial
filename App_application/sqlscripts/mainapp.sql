71c8b40d80547e8f50e7d1aa6928d57944d0df40
Аlex Kukhtin <alex.kukhtin@outlook.com>
2022-06-04 08:28:16 +0300

    update platform

--- App_application/sqlscripts/mainapp.sql
+++ App_application/sqlscripts/mainapp.sql
@@ -1,6 +1,6 @@
 ﻿/*
 version: 1.0.0001
-generated: 01.06.2022 18:18:00
+generated: 04.06.2022 07:33:53
 */
 
 
@@ -8,7 +8,7 @@ generated: 01.06.2022 18:18:00
 
 /*
 version: 10.0.7779
-generated: 01.06.2022 15:54:12
+generated: 03.06.2022 09:37:09
 */
 
 set nocount on;
@@ -4407,12 +4407,67 @@ go
 -- Home model
 -----------------------------------------------
 create or alter procedure app.[Report.Item.Rem.Load]
-@UserId bigint
+@UserId bigint,
+@Date date = null
 as
 begin
 	set nocount on;
 	set transaction isolation level read uncommitted;
 
-	select [Home!THome!Object] = null, [User!TUser!RefId] = @UserId;
+	set @Date = isnull(@Date, getdate());
+
+	with T as (
+		select Qty = sum(Qty * InOut), Item 
+		from app.Journal
+		where [Date] <= @Date
+		group by Item
+	)
+	select [RepData!TRepData!Array] = null, Qty,
+		[Item.Id!TItem!] = T.Item, [Item.Name!TItem!] = isnull(i.[Name], N'Unknown')
+	from T
+	left join app.Items i on T.Item = i.Id;
+
+
+	select [Query!TQuery!Object] = null,
+		[Date] = @Date;
 end
 go
+
+create or alter procedure app.[Report.Item.Sales.Load]
+@UserId bigint,
+@From date = null,
+@To date = null
+as
+begin
+	set nocount on;
+
+	set @From = isnull(@From, N'20220101');
+	set @To = isnull(@To, N'20221231');
+
+	with T as (
+		select Qty = sum(Qty), [Sum] = sum(j.[Sum]), d.Agent, j.Item,
+			GrpAgent = grouping(d.Agent),
+			GrpItem = grouping(j.Item)
+		from app.Journal j 
+		inner join app.Documents d on j.Document = d.Id
+		where d.Kind = N'WAYBILLOUT' and j.[Date] >= @From and j.[Date] <= @To
+		group by rollup(d.Agent, j.Item)
+	)
+	select [RepData!TRepData!Group] = null, Qty, [Sum], AgentId = Agent, ItemId = Item,
+		[Id!!Id] = cast(Agent as nvarchar) + N'_' + cast(Item as nvarchar),
+		[AgentId!!GroupMarker] = GrpAgent,
+		[ItemId!!GroupMarker] = GrpItem,
+		[Items!TRepData!Items] = null,
+		[Item.Id!TItem!Id] = T.Item, [Item.Name!TItem!Name] = i.[Name],
+		[Agent.Id!TAgent!Id] = T.Agent, [Agent.Name!TAgent!Name] = a.[Name]
+	from T
+		left join app.Items i on T.Item = i.Id
+		left join app.Agents a on T.Agent = a.Id
+	order by GrpAgent desc, GrpItem desc;
+
+	select [Query!TQuery!Object] = null,
+		[Period.From!TPeriod!] = @From, [Period.To!TPeriod!] =@To;
+end
+go
+
+exec app.[Report.Item.Sales.Load] 99;
